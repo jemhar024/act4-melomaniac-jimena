f32173861207d0c35e30f5580dcf973ddeaca112
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-18 16:43:25 +0800

    Second Commit

---
+++ song2.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Station </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                 <h1 class="fw-bolder">Welcome to Music Station</h1>
+                <p class="lead">"Music is the wine that fills the cup of silence"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Dating Tayo</h2>";
+                        echo "<p class='lead '>TJ Monterde</p>";
+
+                         echo "<br/>";
+                         
+                
+                        echo "<p> Lagi nalang ganito </p>";
+                        echo "<p> Isipan ay gulong gulo</p>";
+                        echo "<p> Lagi nalang nabibigo</p>";
+                        echo "<p> Ngunit ikaw pa rin, sigaw ng puso</p>";
+                        echo "<br/>";
+
+                        echo "<p> Ilang liham na ang sinulat sayo</p>";
+                        echo "<p> Ilang luha na rin ang natuyo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kailan kaya muling makakatawang</p>";
+                        echo "<p> Hindi ko pinipilit, walang lungkot na sumisilip?</p>";
+                        echo "<p> Kailan kaya muling makakamit</p>";
+                        echo "<p> Ang iyong yakap at halik nang hindi sa panaginip?</p>";
+                        echo "<p> Kailaaan? Kailaaaan? Kailaaaan ang dating tayo?</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kung ano man ang totoo</p>";
+                        echo "<p> Isip man ay litong lito</p>";
+                        echo "<p> Handang handa akong sumalo</p>";
+                        echo "<p> Pagkat ikaw pa rin sigaw ng puso</p>";
+                        echo "<br/>";
+
+                        echo "<p> Ilang awit na ang inalay sayo</p>";
+                        echo "<p> Ilang luha na rin ang natuyo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kailan kaya muling makakatawang</p>";
+                        echo "<p> Hindi ko pinipilit, walang lungkot na sumisilip</p>";
+                        echo "<p> Kailan kaya muling makakamit</p>";
+                        echo "<p> Ang iyong yakap at halik ng hindi sa panaginip?</p>";
+                        echo "<p> Kailaaan? Kailaaaan? Kailaaaan? (kailan) kailan</p>";
+                        echo "<p> Kailaaan ang dating...</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kailan kaya muling matatamasang?</p>";
+                        echo "<p> Ikaw ay makasama at sabay tayong kakanta</p>";
+                        echo "<p> Kailan kaya muling mararanasan?</p>";
+                        echo "<p> Sa pagdilat ng mata, ika'y hindi lang ala-ala</p>";
+                        echo "<br/>";
+
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jemhar Jhon L. Logatoc</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
