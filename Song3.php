430a288f1f2f7afe864d736abe997bd7111fd26f
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-18 16:48:20 +0800

    Fourth Commit

--- Song3.php
+++ Song3.php
@@ -134,7 +134,7 @@
         </section>
 
         <footer class="py-4 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Luz </p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kerby Jimena </p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
---
+++ song4.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Station </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                 <h1 class="fw-bolder">Welcome to Music Station</h1>
+                <p class="lead">"Music is the wine that fills the cup of silence"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Ang Huling El Bimbo</h2>";
+                        echo "<p class='lead '>Eraserheads</p>";
+
+                        
+                
+                        echo "<p> Kamukha mo si Paraluman</p>";
+                        echo "<p> Nung tayo ay bata pa</p>";
+                        echo "<p> At ang galing galing mong sumayaw</p>";
+                        echo "<p> Mapa boogie man o cha cha</p>";
+                        echo "<p> Ngunit ang paborito</p>";
+                        echo "<p> Ay ang pagsayaw mo ng El Bimbo</p>";
+                        echo "<p> Nakakaindak, nakakaaliw</p>";
+                        echo "<p> Nakakatindig balahibo</p>";
+                        echo "<p> Pagkaggaling sa eskwela</p>";
+                        echo "<p> Ay dideretso na sa inyo</p>";
+                        echo "<p> At buong maghapon ay tinuturuan mo ako</p>";
+                        echo "<br/>";
+
+                        echo "<p> Magkahawak ang ating kamay</p>";
+                        echo "<p> At walang kamalaymalay</p>";
+                        echo "<p> Na tinuruan mo ang puso ko</p>";
+                        echo "<p> Na umibig ng tunay</p>";
+                        echo "<br/>";
+
+                        
+                        echo "<p> Naninigas ang aking katawan</p>";
+                        echo "<p> Kapag umikot na ang plaka</p>";
+                        echo "<p> Patay sa kembot ng beywang mo</p>";
+                        echo "<p> At pungay ng yong mga mata</p>";
+                        echo "<p> Lumiliwanag ang buhay</p>";
+                        echo "<p> Habang tayo'y magkaakbay</p>";
+                        echo "<p> At dahang dahang dumudulas</p>";
+                        echo "<p> Ang kamay ko sa makinis mong braso</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sana noon pa man ay sinabi na sa iyo</p>";
+                        echo "<p> At kahit hindi na uso ay ito lang ang alam ko</p>";
+                        echo "<br/>";
+
+                        echo "<p> Magkahawak ang ating kamay</p>";
+                        echo "<p> At walang kamalaymalay</p>";
+                        echo "<p> Na tinuruan mo ang puso ko</p>"; 
+                        echo "<p> Na umibig ng tunay</p>"; 
+                        echo "<br/>";
+
+                        echo "<p> La la la la la...</p>";
+                        echo "<br/>";
+
+                        echo "<p> At lumipas ang maraming taon</p>";
+                        echo "<p> Hindi na tayo nagkita</p>";
+                        echo "<p> Balita ko'y may anak ka na</p>";
+                        echo "<p> Ngunit walang asawa</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tagahugas ka raw ng pinggan sa may Ermita</p>";
+                        echo "<p> At isang gabi'y nasagasaan sa isang madilim na eskenita</p>";
+                        echo "<p> Lahat ng pangarap ko'y bigla lang natunaw</p>";
+                        echo "<p> Sa panaginip na lang pala kita maisasayaw</p>";
+                        echo "<br/>";
+
+                        echo "<p> Magkahawak ang ating kamay</p>";
+                        echo "<p> At walang kamalaymalay</p>";
+                        echo "<p> Na tinuruan mo ang puso ko</p>"; 
+                        echo "<p> Na umibig ng tunay</p>"; 
+                        echo "<br/>";
+
+                        echo "<p> Magkahawak ang ating kamay</p>";
+                        echo "<p> At walang kamalaymalay</p>";
+                        echo "<p> Na tinuruan mo ang puso ko</p>"; 
+                        echo "<p> Na umibig ng tunay</p>"; 
+                        echo "<br/>";
+
+                        echo "<p> La la la la la...</p>";
+                        echo "<br/>";
+
+
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kerby Jimena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
