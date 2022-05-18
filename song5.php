418440ae03e855c7e2b81c777f97f90a0c286aae
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-18 16:49:55 +0800

    Last Commit

---
+++ song5.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Station </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                 <h1 class="fw-bolder">Welcome to Music Station</h1>
+                <p class="lead">"Music is the wine that fills the cup of silence"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>One Call Away</h2>";
+                        echo "<p class='lead '>Charlie Puth</p>";
+                        echo "<br/>";
+
+                        
+                         echo "<p>I'm only one call away</p>";
+                         echo "<p>I'll be there to save the day</p>";
+                         echo "<p>Superman got nothing on me</p>";
+                         echo "<p>I'm only one call away</p>";
+                         echo "<br/>";
+
+                         echo "<p>Call me, baby, if you need a friend</p>";
+                         echo "<p>I just wanna give you love</p>";
+                         echo "<p>C'mon, c'mon, c'mon</p>";
+                         echo "<p>Reaching out to you, so take a chance</p>";
+                         echo "<br/>";
+
+                         echo "<p>No matter where you go</p>";
+                         echo "<p>Know you're not alone</p>";
+                         echo "<br/>";
+
+                         echo "<p>I'm only one call away</p>";
+                         echo "<p>I'll be there to save the day</p>";
+                         echo "<p>Superman got nothing on me</p>";
+                         echo "<p>I'm only one call away</p>";
+                         echo "<br/>";
+
+                         echo "<p>Come along with me and don't be scared</p>";
+                         echo "<p>I just wanna set you free</p>";
+                         echo "<p> C'mon, c'mon, c'mon</p>";
+                         echo "<p>You and me can make it anywhere</p>";
+                         echo "<p> But for now, we can stay here for a while</p>";
+                         echo "<p>'Cause you know, I just wanna see you smile</p>";
+                        echo "<br/>";
+
+                         echo "<p>No matter where you go</p>";
+                         echo "<p>You know you're not alone</p>";
+                        echo "<br/>";
+
+                         echo "<p>I'm only one call away</p>";
+                         echo "<p> I'll be there to save the day</p>";
+                         echo "<p>Superman got nothing on me</p>";
+                         echo "<p>I'm only one call away</p>";
+                         echo "<br/>";
+
+                         echo "<p>And when you're weak I'll be strong</p>";
+                         echo "<p>I'm gonna keep holding on</p>";
+                         echo "<p>Now don't you worry, it won't be long</p>";
+                         echo "<p>Darling, and when you feel like hope is gone</p>";
+                         echo "<p>Just run into my arms</p>";
+                        echo "<br/>";
+
+                         echo "<p>I'm only one call away</p>";
+                         echo "<p>I'll be there to save the day</p>";
+                         echo "<p>Superman got nothing on me</p>";
+                         echo "<p>I'm only one, I'm only one call away</p>";
+                         echo "<p>I'll be there to save the day</p>";
+                         echo "<p>Superman got nothing on me</p>";
+                         echo "<p>I'm only one call away</p>";
+                        echo "<br/>";
+
+                        echo "<p> I'm only one call away</p>";
+                        echo "<br/>";
+
+
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kerby Jimena</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
