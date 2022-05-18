217637ea70402c92cb622c8f1362fb9290915c16
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-05-18 16:46:50 +0800

    Third Commit

---
+++ Song3.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> SMART RECORDS </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to SMART RECORDS</h1>
+                <p class="lead">“Keep a song in your heart here in SMART RECORDS.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Pink skies</h2>";
+                        echo "<p class='lead '>Lany</p>";
+                        echo "<br/>";
+                
+
+                        echo "<p> [Verse 1]</p>";
+                        echo "<p> Thrift store fashion</p>";
+                        echo "<p> Imperfect tattoos</p>";
+                        echo "<p> Taking showers</p>";
+                        echo "<p> Minus shampoo</p>";
+                        echo "<br/>";
+
+
+                        //Pre-Chorus
+                        echo "<p> [Pre-Chorus]</p>";
+                        echo "<p> You are my favorite everything</p>";
+                        echo "<p> Been telling girls that since I was 16</p>";
+                        echo "<p> Shut up, I love you</p>";
+                        echo "<p> You're my best friend</p>";
+                        echo "<br/>";
+
+
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Get ya under pink skies, I know exactly where we should go</p>";
+                        echo "<p> 'Cause I love the way your green eyes mix with that Malibu indigo</p>";
+                        echo "<p> Talking under pink skies, I think our hearts are starting to show</p>";
+                        echo "<p> That it's better, you and I, under pink skies</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> [Verse 2]</p>";
+                        echo "<p> Underwear, I kinda care</p>";
+                        echo "<p> Wanna look good for you</p>";
+                        echo "<p> British bomb, you turn me on</p>";
+                        echo "<p> No matter what ya do</p>";
+                        echo "<br/>";
+
+                        //Pre-Chorus
+                        echo "<p> [Pre-Chorus]</p>";
+                        echo "<p> You are my favorite everything</p>";
+                        echo "<p> Been telling you that since 2015</p>";
+                        echo "<p> Shut up, I love you</p>";
+                        echo "<p> You're my best friend</p>";
+                        echo "<br/>";
+
+
+                       //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Get ya under pink skies, I know exactly where we should go</p>";
+                        echo "<p> 'Cause I love the way your green eyes mix with that Malibu indigo</p>";
+                        echo "<p> Talking under pink skies, I think our hearts are starting to show</p>";
+                        echo "<p> That it's better, you and I, under pink skies</p>";
+                        echo "<br/>";
+
+
+
+                        echo "<br/>";
+                        echo "<p> [Bridge]</p>";
+                        echo "<p> We can work it out</p>";
+                        echo "<p> You and I are meant to be together</p>";
+                        echo "<p> This is how it's supposed to feel</p>";
+                        echo "<p> I'm in love with how this feels</p>";
+                        echo "<br/>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> [Chorus]</p>";
+                        echo "<p> Get ya under pink skies, I know exactly where we should go</p>";
+                        echo "<p> 'Cause I love the way your green eyes mix with that Malibu indigo</p>";
+                        echo "<p> Talking under pink skies, I think our hearts are starting to show</p>";
+                        echo "<p> That it's better, you and I, under pink skies</p>";
+                        echo "<p> Get you under pink skies, I know exactly where we should go</p>";
+                        echo "<p> 'Cause I love the way your green eyes mix with that Malibu indigo</p>";
+                        echo "<p> Talking under pink skies, I think our hearts are starting to showw</p>";
+                        echo "<p> And it's better, you and I, under pink skies</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Christine Luz </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
